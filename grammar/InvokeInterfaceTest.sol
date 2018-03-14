f1f4ccef66abff5f2308651547dd9ee860b012cc
wanglei <wanglei@wangleideMacBook-Pro.local>
2018-03-14 12:00:18 +0800

    只有合约创建者才能设置地址

--- grammar/InvokeInterfaceTest.sol
+++ grammar/InvokeInterfaceTest.sol
@@ -1,12 +1,24 @@
 pragma solidity ^0.4.0;
 
+
 contract TestInterface {
     function add(uint a,uint b) public pure returns (uint);
-}
-contract InvokeInterFace{
-    function invoke(address _address) public pure returns(uint){
-        TestInterface ti = TestInterface(_address);
-        uint c =ti.add(1,2);
+  }
+contract InvokeInterFace001{
+    address public owner;
+    TestInterface public ti;
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    function InvokeInterFace001() public {
+      owner = msg.sender;
+    }
+    function setTestFunc(address _address) public onlyOwner {
+      ti = TestInterface(_address);
+    }
+    function invoke(uint a,uint b) public view returns(uint){
+        uint c =ti.add(a,b);
         return c;
     }
 }
